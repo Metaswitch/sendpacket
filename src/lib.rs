cfa0d586dc943bfa28376932a010b7e9b5be024f
Jaime Valdemoros <jaime.valdemorosgomez@metaswitch.com>
2018-05-10 21:30:10 +0100

    Ip over tcp minimal example

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,34 @@
+use std::ops::Div;
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Ip {
+    dst: String
+}
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Tcp;
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct IpOverTcp {
+    dst: String,
+}
 
+impl Div<Tcp> for Ip {
+    type Output = IpOverTcp;
 
+    fn div(self, _rhs: Tcp) -> Self::Output {
+        IpOverTcp {
+            dst: self.dst
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        println!("It works!")
+        assert_eq!(IpOverTcp { dst: "blah".to_string() }, Ip { dst: "blah".to_string()} / Tcp )
     }
 }
