e1d68681b01fe23ed8acc0814a863aac7605d073
Blake Hawkins <blake.hawkins.11@gmail.com>
2018-05-11 15:23:21 +0100

    rename to sendpacket

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[macro_use]
-extern crate lib_hack12;
-use lib_hack12::*;
+extern crate lib_sendpacket;
+use lib_sendpacket::*;
 
 fn main() {
     let p = ether!(src_mac = [10,1,1,1,1,1], dst_mac = [10,1,1,1,1,2]) / mpls!(label = 77u32) / ip!(dst = "127.0.0.1", src = "10.8.0.1") / tcp!(dport = 85u16, sport = 10u16) / payload!(vec![1,2,3,4]);
