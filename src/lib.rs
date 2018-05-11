b2ca7aad93ad376a1fccc5435c2d03b23cdeef5e
Jaime Valdemoros <jaime.valdemorosgomez@metaswitch.com>
2018-05-11 15:37:07 +0100

    Initial ipv4 header work

--- src/lib.rs
+++ src/lib.rs
@@ -7,19 +7,22 @@ extern crate ipnetwork;
 extern crate pnet;
 
 use ipnetwork::IpNetwork;
+use pnet::datalink::{Channel, MacAddr, NetworkInterface};
 use pnet::packet::{MutablePacket, Packet};
 use pnet::packet::arp::{ArpHardwareTypes, ArpOperation, ArpOperations};
 use pnet::packet::ethernet::{EtherType, EtherTypes};
 use pnet::packet::ethernet::MutableEthernetPacket;
-use pnet::packet::ip;
+use pnet::packet::ip::{self, IpNextHeaderProtocol, IpNextHeaderProtocols};
 use pnet::packet::ipv4;
-use pnet::datalink::MacAddr;
+use pnet::packet::ipv4::MutableIpv4Packet;
+
 use std::fs;
 use std::io::Read;
 use std::num::ParseIntError;
 use std::ops::Div;
 use std::path::Path;
 use std::str::FromStr;
+use std::marker::PhantomData;
 
 ///
 /// Macros
@@ -122,6 +125,9 @@ pub struct Ether {
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, new)]
+pub struct EtherWrapper(Ether);
+
+#[derive(Clone, Debug, PartialEq, Eq, new)]
 pub struct MPLS {
     pub label: u32,
 }
@@ -173,8 +179,8 @@ pub struct L2 {
     mpls_labels: Vec<MPLS>,
 }
 
-impl PackageHeader for Ether {
-    fn build_header(&self, payload: &[u8]) -> Vec<u8> {
+impl Ether {
+    fn build_header_inner(&self, payload: &[u8], ether_type: Option<EtherType>) -> Vec<u8> {
         let ether_buffer_len = payload.len() + 38; // 42 if with 802.1Q tags
         let mut ether_buffer = vec![0u8; ether_buffer_len];
         let mut ether_packet = MutableEthernetPacket::new(&mut ether_buffer).unwrap();
@@ -182,18 +188,33 @@ impl PackageHeader for Ether {
         ether_packet.set_source(self.src_mac.to_mac_addr());
         ether_packet.set_destination(self.dst_mac.to_mac_addr());
 
-//        ether_packet.set_ethertype(EtherTypes::);
+        if let Some(t) = ether_type {
+            ether_packet.set_ethertype(t)
+        }
+
         ether_packet.set_payload(payload);
         ether_packet.packet().to_vec()
     }
 }
 
-impl PackageHeader for L2 {
-    fn build_header(&self, payload: &[u8]) -> Vec<u8> {
+impl PackageHeader<EtherType> for Ether {
+    fn build_header(&self, payload: &[u8], ether_type: EtherType) -> Vec<u8> {
+        self.build_header_inner(payload, Some(ether_type))
+    }
+}
+
+impl PackageHeader<()> for Ether {
+    fn build_header(&self, payload: &[u8], _extra: ()) -> Vec<u8> {
+        self.build_header_inner(payload, None)
+    }
+}
+
+impl PackageHeader<EtherType> for L2 {
+    fn build_header(&self, payload: &[u8], ether_type: EtherType) -> Vec<u8> {
         let l2_packet: Vec<u8> = vec![];
         // Insert RLC function here for L2 packets
 
-        self.ether.build_header(&l2_packet)
+        self.ether.build_header(&l2_packet, ether_type)
     }
 }
 
@@ -223,12 +244,12 @@ pub struct L3 {
     ip: Ip, // if you allow more types here, they must implement EtherTypeable
 }
 
-impl PackageHeader for L3 {
-    fn build_header(&self, payload: &[u8]) -> Vec<u8> {
+impl PackageHeader<IpNextHeaderProtocol> for L3 {
+    fn build_header(&self, payload: &[u8], next_header: IpNextHeaderProtocol) -> Vec<u8> {
         let l3_packet: Vec<u8> = vec![];
         // Insert RLC function here for L3 packets
 
-        self.l2.build_header(&l3_packet)
+        self.l2.build_header(&l3_packet, self.ip.ether_type())
     }
 }
 
@@ -262,18 +283,35 @@ pub trait Transport {}
 
 impl Transport for Tcp {}
 
+pub struct Udp {
+    src_port: u16,
+    dst_port: u16,
+}
+
+impl Transport for Udp {}
+
 #[derive(Clone, Debug, PartialEq, Eq, new)]
 pub struct L3Over<T: Transport> {
     l3: L3,
     transport: T,
 }
 
-impl PackageHeader for L3Over<Tcp> {
-    fn build_header(&self, payload: &[u8]) -> Vec<u8> {
+impl PackageHeader<()> for L3Over<Tcp> {
+    fn build_header(&self, payload: &[u8], _extra: ()) -> Vec<u8> {
         let l3_over_tcp_packet: Vec<u8> = vec![];
         // Insert RLC function here for L3 over TCP packets
+        panic!("At the disco");
+        self.l3.build_header(&l3_over_tcp_packet, IpNextHeaderProtocols::Tcp)
+    }
+}
 
-        self.l3.build_header(&l3_over_tcp_packet)
+
+impl PackageHeader<()> for L3Over<Udp> {
+    fn build_header(&self, payload: &[u8], _extra: ()) -> Vec<u8> {
+        let l3_over_udp_packet: Vec<u8> = vec![];
+        // Insert RLC function here for L3 over TCP packets
+
+        self.l3.build_header(&l3_over_udp_packet, IpNextHeaderProtocols::Udp)
     }
 }
 
@@ -285,31 +323,37 @@ impl<T: Transport> Div<T> for L3 {
     }
 }
 
-pub trait PackageHeader {
-    fn build_header(&self, payload: &[u8]) -> Vec<u8>;
+pub trait PackageHeader<ExtraInfo> {
+    fn build_header(&self, payload: &[u8], ether_type: ExtraInfo) -> Vec<u8>;
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, new)]
-pub struct Package<H: PackageHeader> {
+pub struct Package<Extra, H: PackageHeader<Extra>> {
     header: H,
     payload: Payload,
+    #[new(default)] _phantom: PhantomData<Extra>,
 }
 
-impl<H: PackageHeader> Package<H> {
-    pub fn build_packet(&self) -> Vec<u8> {
-        self.header.build_header(&self.payload.payload)
+pub trait BuildPacket {
+    fn build_packet(&self) -> Vec<u8>;
+}
+
+impl<H: PackageHeader<()>> BuildPacket for Package<(), H> {
+    fn build_packet(&self) -> Vec<u8> {
+        self.header.build_header(&self.payload.payload, ())
     }
 }
 
 macro_rules! payload_div {
     ($header:ty) => {
         impl Div<Payload> for $header {
-            type Output = Package<Self>;
+            type Output = Package<(), Self>;
 
             fn div(self, rhs: Payload) -> Self::Output {
                 Package {
                     payload: rhs,
                     header: self,
+                    _phantom: PhantomData,
                 }
             }
         }
@@ -317,9 +361,9 @@ macro_rules! payload_div {
 }
 
 payload_div!(Ether);
-payload_div!(L2);
-payload_div!(L3);
-payload_div!(L3Over<Tcp>);
+//payload_div!(L2);
+//payload_div!(L3);
+payload_div!(L3Over<Udp>);
 
 impl FromStr for Mac {
     type Err = ParseIntError;
@@ -415,8 +459,8 @@ mod tests {
 }
 
 mod builder {
-    use L2;
     use Ip;
+    use L2;
     use Tcp;
 
     fn build_ethernet_pkt(ether: L2, payload: [u8; 1024]) -> [u8; 1024] {
